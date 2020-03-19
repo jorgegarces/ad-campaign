366b917d4e77bb02d47ba8c9a4e4f2b5770c35d4
Jorge Garcés <jgarcesembarba@gmail.com>
2020-03-20 00:07:01 +0100

    feat: in memory repo does not allow more than one ad with the same title and description

--- src/test/java/infrastructure/InMemoryAdRepositoryShould.java
+++ src/test/java/infrastructure/InMemoryAdRepositoryShould.java
@@ -2,6 +2,7 @@ package infrastructure;
 
 import domain.ad.Ad;
 import domain.exceptions.AdDoesNotExistException;
+import domain.exceptions.DuplicateAdException;
 import infrastructure.inMemory.InMemoryAdRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,5 +35,18 @@ public class InMemoryAdRepositoryShould {
     public void throw_an_error_when_trying_to_remove_an_ad_that_does_not_exist() throws RuntimeException {
 
         Assertions.assertThrows(AdDoesNotExistException.class, () -> testRepo.remove(testAd.getId()));
+    }
+
+    @Test
+    public void not_allow_more_than_one_ad_with_same_title_and_description() throws RuntimeException {
+
+        Ad testAd2 = new Ad.AdBuilder()
+                .title("Test ad title")
+                .description("Test ad description")
+                .publicationDate(LocalDate.of(2019, 10, 1))
+                .build();
+        testRepo.add(testAd);
+
+        Assertions.assertThrows(DuplicateAdException.class, () -> testRepo.add(testAd2));
     }
 }
