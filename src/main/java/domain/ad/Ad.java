36a49fae2284a97ace0e5ad8800a7a7619bd8c7e
Jorge Garcés <jgarcesembarba@gmail.com>
2020-03-19 23:50:32 +0100

    feat: not allow ad to build without a title

---
+++ src/main/java/domain/ad/Ad.java
@@ -0,0 +1,81 @@
+package domain.ad;
+
+import domain.ad.dto.AdDTO;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Ad {
+
+    private final AdId id;
+    private final String title;
+    private final String description;
+    private final LocalDate publicationDate;
+
+    private Ad(AdBuilder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.description = builder.description;
+        this.publicationDate = builder.publicationDate;
+    }
+
+    public AdId getId() {
+        return this.id;
+    }
+
+    public AdDTO createDTO() {
+        AdDTO adDTO = new AdDTO();
+        adDTO.title = this.title;
+        adDTO.description = this.description;
+        adDTO.date = this.publicationDate;
+
+        return adDTO;
+    }
+
+    public static final class AdBuilder {
+
+        private AdId id;
+        private String title;
+        private String description;
+        private LocalDate publicationDate;
+
+        public AdBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public AdBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public AdBuilder publicationDate(LocalDate date) {
+            this.publicationDate = date;
+            return this;
+        }
+
+        public Ad build() {
+            this.id = new AdId();
+            if (title == null) throw new IllegalStateException("Title cannot be empty");
+            if (description == null) throw new IllegalStateException("Description cannot be empty");
+            if (publicationDate == null) throw new IllegalStateException("Publication date cannot be empty");
+            if (title.length() > 50) throw new IllegalStateException("Title must be max 50 characters long");
+            if (title.equals(description)) throw new IllegalStateException("Title and description cannot be the same");
+            return new Ad(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ad ad = (Ad) o;
+        return Objects.equals(title, ad.title) &&
+                Objects.equals(description, ad.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description);
+    }
+}
---
+++ src/test/java/domain/AdShould.java
@@ -0,0 +1,27 @@
+package domain;
+
+import domain.ad.Ad;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class AdShould {
+
+    @Test
+    public void not_allow_building_without_a_title() {
+
+        String expected = "";
+
+        try {
+            new Ad.AdBuilder()
+                    .description("Test ad description")
+                    .publicationDate(LocalDate.of(2019, 10, 1))
+                    .build();
+        } catch (IllegalStateException e) {
+            expected = e.getMessage();
+        }
+
+        Assert.assertEquals("Title cannot be empty", expected);
+    }
+}
